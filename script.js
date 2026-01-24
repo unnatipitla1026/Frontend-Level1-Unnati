818b5e57ad74b4a2e83bc3d53f1a9a267e5c20ad
Unnati Pitla <unnatipitla1026@gmail.com>
2026-01-24 20:58:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 /* HAMBURGER */
-  const hamburger = document.getElementById("hamburger");
+const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("navLinks");
 
+// Hamburger toggle
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+});
+
+// Close menu after clicking link (mobile)
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 });
